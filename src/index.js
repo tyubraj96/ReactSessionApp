1dde5097c5e0086ec93091c57b9eda3c1e626531
tyubraj96 <131554500+tyubraj96@users.noreply.github.com>
2024-03-17 14:10:11 +0545

    usestate added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useState} from 'react';
+import './style.css';
+import Test from './test';
+import styles from './index.module.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(styles);
+
+function Theme(){
+         const [theme,setTheme]=useState(styles.light);
+         console.log(theme);
+      
+         const toDark =()=> setTheme(styles.dark);
+         const toLight =()=> setTheme(styles.light);
+         return (
+                  <div className={theme}>
+                           {theme === styles.light ? (<button onClick={toDark}>Convert to Dark</button>) 
+                           :
+                           (<button onClick={toLight}>Convet to Light</button>)}
+                          <Test />
+
+                  </div>
+
+         );
+
+
+}
+
+// import { useState } from 'react';//react provide the object in which we have to distucture useState so curly braces is used
+// function App(){
+//   const [state, setState] =useState(0);//useState is a hook
+
+//   console.log(state);
+//   const increment = ()=>{
+//     setState(state + 1);
+//   };
+//   return(
+//     <div>
+//       <button onClick={increment}>Increment</button>
+//     </div>
+//   )
+// }
+const root = document.getElementById('root');
+const rootElement = ReactDOM.createRoot(root);
+rootElement.render(<Theme />);
+
+
---
+++ src/test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import style from './index.module.css'
+console.log(style);
+const Test =() =>{
+         return<div>Test</div>;
+};
+export default Test;
